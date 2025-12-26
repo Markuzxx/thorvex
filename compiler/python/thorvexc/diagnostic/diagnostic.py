3a96dbd135a173e9665154caae82a59cea5c5ee8
Markuzxx <marco.combi2008@gmail.com>
2025-12-26 22:35:34 +0100

    Add Diagnostic classes for error handling and reporting

---
+++ compiler/python/thorvexc/diagnostic/diagnostic.py
@@ -0,0 +1,67 @@
+from __future__ import annotations
+
+from enum import Enum, StrEnum, auto
+from dataclasses import dataclass
+
+
+class DiagnosticSeverity(Enum):
+
+    ERROR = auto()
+    WARNING = auto()
+    INFO = auto()
+
+
+class DiagnosticCode(StrEnum):
+
+    # Errors
+    ...
+
+    # Warnings
+    ...
+
+    # Infos
+    ...
+
+
+@dataclass()
+class Diagnostic:
+
+    severity: DiagnosticSeverity
+    code: DiagnosticCode
+    message: str
+    snippet: str
+    source: str
+    line: int
+    column_start: int
+    column_end: int
+
+    def __str__(self) -> str:
+
+        # Colors
+        severity_colors: dict[DiagnosticSeverity, str] = {
+            DiagnosticSeverity.ERROR: "\033[31m",   # Red
+            DiagnosticSeverity.WARNING: "\033[33m", # Yellow
+            DiagnosticSeverity.INFO: "\033[36m"     # Cyan
+        }
+        color = severity_colors.get(self.severity, "\033[0m")
+        reset = "\033[0m"
+        gutter_color = "\033[90m" # Gray
+
+        # Gutter
+        line_gutter = f"  {self.line} | "
+        empty_gutter = ' ' * len(line_gutter[:-2]) + "| "
+
+        # Split snippet
+        snippet_prefix = self.snippet[:(self.column_start - 1)]
+        snippet_highlight = self.snippet[(self.column_start - 1):self.column_end]
+        snippet_suffix = self.snippet[self.column_end:]
+
+        # Caret
+        caret_padding = ' ' * (self.column_start - 1)
+        caret = '^' * (self.column_end - self.column_start + 1)
+
+        return (f"{color}{self.severity.name.lower()}[{self.code}]: {self.message}\n"
+                f"{gutter_color}  --> {self.source}:{self.line}:{self.column_start}\n"
+                f"{empty_gutter}\n"
+                f"{line_gutter}{reset}{snippet_prefix}{color}{snippet_highlight}{reset}{snippet_suffix}\n"
+                f"{gutter_color}{empty_gutter}{caret_padding}{color}{caret}")
