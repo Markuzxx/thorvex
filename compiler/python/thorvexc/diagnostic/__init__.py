f9dc5d591c79a16fb1f72ba8164c55081f30f0f5
Markuzxx <marco.combi2008@gmail.com>
2025-12-26 23:08:12 +0100

    Add exports to __init__.py for diagnostic and manager modules

---
+++ compiler/python/thorvexc/diagnostic/__init__.py
@@ -0,0 +1,13 @@
+from .diagnostic import DiagnosticSeverity, DiagnosticCode, Diagnostic, ThorvexError, ThorvexWarning
+from .manager import DiagnosticManager
+
+
+__all__ = [
+
+    # diagnostic.py
+    "DiagnosticSeverity", "DiagnosticCode", "Diagnostic", "ThorvexError", "ThorvexWarning",
+
+    # manager.py
+    "DiagnosticManager",
+
+]
