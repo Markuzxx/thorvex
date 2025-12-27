7c6fc5f8e786f7fea36dfa1a6bfeaa582928b6ce
Markuzxx <marco.combi2008@gmail.com>
2025-12-27 13:27:02 +0100

    #2 compiler.python.thorvexc.diagnostic: move ThorvexError from diagnostic.py to errors.py.

--- compiler/python/thorvexc/diagnostic/diagnostic.py
+++ compiler/python/thorvexc/diagnostic/diagnostic.py
@@ -71,20 +71,6 @@ class Diagnostic:
                 f"{gutter_color}{empty_gutter}{caret_padding}{color}{caret}{reset}\n")
 
 
-class ThorvexError(Diagnostic):
-
-    def __init__(self,
-                 code: DiagnosticCode,
-                 message: str,
-                 snippet: str,
-                 source: str,
-                 line: int,
-                 column_start: int,
-                 column_end: int) -> None:
-
-        super().__init__(DiagnosticSeverity.ERROR, code, message, snippet, source, line, column_start, column_end)
-
-
 class ThorvexWarning(Diagnostic):
 
     def __init__(self,
