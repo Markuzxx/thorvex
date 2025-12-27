769484741c515b8a2c623fb08b3858794f86e14a
Markuzxx <marco.combi2008@gmail.com>
2025-12-27 13:30:24 +0100

    #4 compiler.python.thorvexc.diagnostic: add initial lexer errors.

--- compiler/python/thorvexc/diagnostic/errors.py
+++ compiler/python/thorvexc/diagnostic/errors.py
@@ -15,3 +15,72 @@ class ThorvexError(Diagnostic):
                  column_end: int) -> None:
 
         super().__init__(DiagnosticSeverity.ERROR, code, message, snippet, source, line, column_start, column_end)
+
+
+########################################################################################################################
+# Lexer errors (1XXX)
+
+class LexerError(ThorvexError):
+
+    def __init__(self,
+                 message: str,
+                 snippet: str,
+                 source: str,
+                 line: int,
+                 column_start: int,
+                 column_end: int,
+                 code: DiagnosticCode = DiagnosticCode.E1000) -> None:
+
+        super().__init__(code, message, snippet, source, line, column_start, column_end)
+
+
+class UnexpectedCharacterError(LexerError):
+
+    def __init__(self,
+                 message: str,
+                 snippet: str,
+                 source: str,
+                 line: int,
+                 column_start: int,
+                 column_end: int) -> None:
+
+        super().__init__(message, snippet, source, line, column_start, column_end, code=DiagnosticCode.E1001)
+
+
+class UnterminatedStringLiteralError(LexerError):
+
+    def __init__(self,
+                 message: str,
+                 snippet: str,
+                 source: str,
+                 line: int,
+                 column_start: int,
+                 column_end: int) -> None:
+
+        super().__init__(message, snippet, source, line, column_start, column_end, code=DiagnosticCode.E1002)
+
+
+class UnterminatedCharacterLiteralError(LexerError):
+
+    def __init__(self,
+                 message: str,
+                 snippet: str,
+                 source: str,
+                 line: int,
+                 column_start: int,
+                 column_end: int) -> None:
+
+        super().__init__(message, snippet, source, line, column_start, column_end, code=DiagnosticCode.E1003)
+
+
+class InvalidEscapeSequenceError(LexerError):
+
+    def __init__(self,
+                 message: str,
+                 snippet: str,
+                 source: str,
+                 line: int,
+                 column_start: int,
+                 column_end: int) -> None:
+
+        super().__init__(message, snippet, source, line, column_start, column_end, code=DiagnosticCode.E1004)
