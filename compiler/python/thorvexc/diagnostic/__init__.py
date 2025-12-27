ba751a859a99de6a0ba2f5380cb33024a8f702fe
Markuzxx <marco.combi2008@gmail.com>
2025-12-27 17:20:32 +0100

    #5 compiler.python.thorvexc.diagnostic: update package exports.

--- compiler/python/thorvexc/diagnostic/__init__.py
+++ compiler/python/thorvexc/diagnostic/__init__.py
@@ -1,13 +1,26 @@
-from .diagnostic import DiagnosticSeverity, DiagnosticCode, Diagnostic, ThorvexError, ThorvexWarning
+from .diagnostic import DiagnosticSeverity, DiagnosticCode, Diagnostic
+
 from .manager import DiagnosticManager
+
+from .errors import (ThorvexError, LexerError, UnexpectedCharacterError, UnterminatedCharacterLiteralError,
+                     UnterminatedStringLiteralError, InvalidEscapeSequenceError)
+
+from .warnings import ThorvexWarning
 
 
 __all__ = [
 
     # diagnostic.py
-    "DiagnosticSeverity", "DiagnosticCode", "Diagnostic", "ThorvexError", "ThorvexWarning",
+    "DiagnosticSeverity", "DiagnosticCode", "Diagnostic",
 
     # manager.py
     "DiagnosticManager",
+
+    # errors.py
+    "ThorvexError", "LexerError", "UnexpectedCharacterError", "UnterminatedCharacterLiteralError",
+    "UnterminatedStringLiteralError", "InvalidEscapeSequenceError",
+
+    # warnings.py
+    "ThorvexWarning",
 
 ]
